44c2eddb2bc49623cb4eebb4e78e0e1b23820d13
PazhenkovaEA <pazhenkova.e@gmail.com>
2021-10-24 19:14:46 +0200

    lethal equivalents

--- popmodel/__init__.py
+++ popmodel/__init__.py
@@ -1,11 +1,7 @@
 #__init__.py
 
-from .reproduction import demoModel
-from .reproduction import bearFather
-from .reproduction import bearMother
 from .mortality import NaturalMortality
 from .mortality import InbreedingLoad
 from .mortality import IBD
 from .stat import CalcStats
 from .stat import CalcStats1
-from .simulation import Simulation
